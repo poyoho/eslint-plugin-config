05429f708016275771327b0b654490f46757cdb4
poyoho <907415276@qq.com>
2021-06-05 12:23:18 +0800

    chore: 删除eslint-ignore

--- playground/project/src/vue/ts/no-route-query.vue
+++ playground/project/src/vue/ts/no-route-query.vue
@@ -5,8 +5,14 @@
 <script lang="ts">
 function aa(this: any, a: number) {
   console.log(a)
-  // eslint-disable-next-line @poyoho/config/vue/no-route-query
   console.log(this.$route.query)
+}
+export default {
+  methods: {
+    aaa() {
+      //
+    }
+  }
 }
 </script>
 
