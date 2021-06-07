dc504727b57a8c23b2d6e04733ba0b0330b5c1d3
poyoho <907415276@qq.com>
2021-06-07 11:35:06 +0800

    feat(test): :white_check_mark: 添加测试用例

--- docs/rules/ensure-scope-block.md
+++ docs/rules/ensure-scope-block.md
@@ -1,17 +1,51 @@
 ## 确保代码块的作用范围 (ensure-scope-block)
 
-// full it
+`if`、`else`、`for`等语句其主体必须包裹在一个代码块内。确保作用范围符合预期，且防止因后期维护原因，导致缩进改变而引起难以排查的 bug。
 
 ## 事例
 
 此规则的**错误**代码示例：
 
-```html
-// full it
+```js
+// 错误示例 1
+if (b) return
+
+// 错误示例 2
+if (b)
+  console.log('当前值为 true')
+else
+  console.log('当前值为 false')
+
+// 错误示例 3
+let isFind = false
+for (const item of arr)
+  if (item.name === '张三') isFind = true
+
+console.log(isFind)
 ```
 
 此规则的**正确**代码示例：
 
-```html
-// full it
+```js
+// 正确示例 1
+if (b) {
+  return
+}
+
+// 正确示例 2
+if (b) {
+  console.log('当前值为 true')
+} else {
+  console.log('当前值为 false')
+}
+
+// 正确示例 3
+let isFind = false
+for (const item of arr) {
+  if (item.name === '张三') {
+    isFind = true
+  }
+}
+
+console.log(isFind)
 ```
--- tests/module/ensure-scope-block.ts
+++ tests/module/ensure-scope-block.ts
@@ -5,8 +5,53 @@ import rule from "../../lib/rules/js/ensure-scope-block"
 export default (ruleTester: RuleTester) =>
   ruleTester.run("ensure-scope-block", rule, {
     valid: [
+      {
+        code: `
+        if (b) {
+          return
+        }
+        `
+      },
+      {
+        code: `
+        if (b) {
+          console.log('当前值为 true')
+        } else {
+          console.log('当前值为 false')
+        }
+        `
+      },
+      {
+        code: `
+        for (const item of arr) {
+          if (item.name === '张三') {
+            isFind = true
+          }
+        }`
+      }
     ],
 
     invalid: [
+      {
+        filename: "test.vue",
+        code: "<script>if (b) return</script>",
+        errors: 1
+      },
+      {
+        code: `
+        if (b)
+          console.log('当前值为 true')
+        else
+          console.log('当前值为 false')
+        `,
+        errors: 2
+      },
+      {
+        code: `
+        for (const item of arr)
+          if (item.name === '张三') isFind = true
+        `,
+        errors: 2
+      }
     ]
   })
